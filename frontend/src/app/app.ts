ba738231d8d407473674be7bced56221f78911dc
DRG-80 <smartecrd.drg@gmail.com>
2025-11-22 18:25:32 +0100

    Añadido componente cards-section

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import { Header } from './componentes/header/header';
 import {Hero} from './hero/hero';
+import {CardsSection} from './cards-section/cards-section';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Hero],
+  imports: [RouterOutlet,Header,Hero,CardsSection],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ frontend/src/app/cards-section/cards-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-section',
+  imports: [],
+  templateUrl: './cards-section.html',
+  styleUrl: './cards-section.scss',
+})
+export class CardsSection {
+
+}
