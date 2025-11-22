a951f1d79cd7eafb43648460b5d8d7d79ef9f5a0
DRG-80 <smartecrd.drg@gmail.com>
2025-11-22 12:23:56 +0100

    Terminado el Hero

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
 import { Header } from './componentes/header/header';
+import {Hero} from './hero/hero';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,],
+  imports: [RouterOutlet,Header,Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  constructor(public router: Router) {}
+
+
 }
